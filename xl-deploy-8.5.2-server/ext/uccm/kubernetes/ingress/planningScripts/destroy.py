080f148fe50f9d29785bd96b7be59c16d212e498
Benoit Moussaud <bmoussaud@xebialabs.com>
2019-02-22 11:09:21 +0100

    fix step descrition

--- xl-deploy-8.5.2-server/ext/uccm/kubernetes/ingress/planningScripts/destroy.py
+++ xl-deploy-8.5.2-server/ext/uccm/kubernetes/ingress/planningScripts/destroy.py
@@ -3,7 +3,7 @@ def step_parameters(port,deployed):
     return {'resource':'ingress', 'resourceName':'{0}-{1}-ingress'.format(deployed.name,port.name), 'order': 43}
 
 def wait_parameters(port,deployed):
-    data = { 'description': 'Wait for Service {1}/{0} deleted on {2}'.format(port.name,deployed.name, deployed.container.name),
+    data = { 'description': 'Wait for Ingress {1}/{0} deleted on {2}'.format(port.name,deployed.name, deployed.container.name),
             'order': 44}
     return data
 
