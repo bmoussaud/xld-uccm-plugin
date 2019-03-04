35b8bc2bca5793375d6ed171c4d71129ede22008
Ravan Naidoo <rnaidoo@xebialabs.com>
2019-03-04 13:07:25 +0100

    Added the concept of a jython processor to manipulate a json definition.
    - Will be used to support profilles for K8s, Cloudformation, Terraform.   Basically any file based deployment system.

---
+++ xl-deploy-8.5.2-server/ext/uccm/utils/profile.py
@@ -0,0 +1,75 @@
+import re
+import json
+import yaml
+from uccm.utils.cfyaml_loader import CfnYamlLoader
+
+
+class ProcessorChain(object):
+    def __init__(self, processors):
+        self.processors = processors
+
+    def process(self, template, profile_dictionary):
+        fcn_to_call = 'process'
+        for p in self.processors:
+            mod_name = "uccm_processors.%s" % p
+            mod = __import__(mod_name, fromlist=["process"])
+            func = getattr(mod, fcn_to_call)
+            template = func(template, profile_dictionary)
+        return template
+
+
+class ProfileProcessor(object):
+
+    def __init__(self, deployed, environment):
+        self.profile_dictionary = ProfileDictionary(environment.profileDictionaries)
+        self.deployed = deployed
+
+    @staticmethod
+    def formatted_json_string(obj):
+        return json.dumps(obj, indent=4, sort_keys=True)
+
+    def read_template(self):
+        template_path = self.deployed.file.path
+        with open(template_path, 'r') as f:
+            raw_data = f.read()
+        if template_path.endswith('yaml'):
+            data = yaml.load(raw_data, Loader=CfnYamlLoader)
+        else:
+            data = json.loads(raw_data)
+        return data
+
+    def process(self):
+        profile_name = self.deployed.profileName
+        template = self.read_template()
+        return ProcessorChain([profile_name]).process(template, self.profile_dictionary)
+
+
+class ProfileDictionary(object):
+
+    def __init__(self, source_dictionaries):
+        self.source_dictionaries = source_dictionaries
+        self.key_pattern = re.compile("\\$\\{this\\.([a-zA-Z0-9]+)\\}")
+
+    def resolve(self, key, default=None):
+        unresolved_value = None
+        for sd in self.source_dictionaries:
+            if key in sd.entries.keys():
+                unresolved_value = sd.entries[key]
+                break
+            elif key in sd.encryptedEntries.keys():
+                unresolved_value = sd.encryptedEntries[key]
+                break
+        if unresolved_value:
+            final_value = unresolved_value
+            match = self.key_pattern.match(unresolved_value)
+            if match:
+                unresolved_keys = match.groups()[1:]
+                for k in unresolved_keys:
+                    resolved_key_value = self.resolve(k)
+                    final_value.replace("${this.%s}" % k, resolved_key_value)
+            return final_value
+        else:
+            if default:
+                return default
+            else:
+                raise Exception("Cannot resolve profile dictionaries. Key'%s'" % key)
