40f0fae67f3440d8810f803e923337d73335345d
Ravan Naidoo <rnaidoo@xebialabs.com>
2019-03-04 13:05:12 +0100

    Added ability to edit python profile processors in the user interface.

--- xl-deploy-8.5.2-server/ext/uccm/restapi/list_templates.py
+++ xl-deploy-8.5.2-server/ext/uccm/restapi/list_templates.py
@@ -1,36 +1,9 @@
-import os
-from os import path
+import uccm.restapi.template_file_store
+reload(uccm.restapi.template_file_store)
 
+from uccm.restapi.template_file_store import TemplateFileStore
 
-class TemplateFileStore(object):
-    def __init__(self):
-        self.base_dir = os.path.join(os.getcwd(), "ext", "uccm_templates")
-
-    def list_template_names(self):
-        onlyfiles = [f for f in os.listdir(self.base_dir) if path.isfile(path.join(self.base_dir, f))]
-        only_json_ftl_files = [f[:-9] for f in onlyfiles if f.endswith(".json.ftl")]
-        return only_json_ftl_files
-
-    def _get_template_path(self, name):
-        if not name.endswith(".json.ftl"):
-            name = "%s.json.ftl" % name
-        return path.join(self.base_dir, name)
-
-    def read_template(self, name):
-        file_path = self._get_template_path(name)
-        if path.isfile(file_path):
-            with open(file_path, "r") as f:
-                return {"name": name, "template": f.read()}
-        else:
-            raise Exception("Template '%s' not found." % name)
-
-    def save_template(self, name, content):
-        file_path = self._get_template_path(name)
-        with open(file_path, "w") as f:
-            f.write(content)
-
-
-if "name" in request.query.keys():
-    response.entity = TemplateFileStore().read_template(request.query['name'])
+if all (k in request.query.keys() for k in ("name",  "type")):
+    response.entity = TemplateFileStore().read_template(request.query['name'], request.query['type'])
 else:
     response.entity = TemplateFileStore().list_template_names()
--- xl-deploy-8.5.2-server/ext/web/uccm/js/uccm.js
+++ xl-deploy-8.5.2-server/ext/web/uccm/js/uccm.js
@@ -1,33 +1,49 @@
 angular
-    .module('uccm', ['ui.ace'])
+    .module('uccm', ['ui.ace', 'ui.bootstrap'])
     .controller(
         'UccmController',
         function($http, $scope, $window) {
             $scope.view_mode = "list";
-            $scope.currentTemplate = null;
+            $scope.currentTemplate = {type: 'py'};
             $scope.templateNames = [];
+            $scope.aceOption = {
+                mode: 'ftl',
+                theme: 'twilight'
+            };
 
             $scope.refreshPage = function() {
                 $window.location.reload();
             };
 
-            $scope.addBlueprint = function() {
-                $scope.currentTemplate = {
+            $scope.addBlueprint = function(type_name) {
+                let template = {
                     name: null,
-                    template: ""
+                    template: "",
+                    type: type_name
+                };
+
+                if (type_name == 'py') {
+                    template.template = "def process(cf_template, profile_dictionary):\n" +
+                                        "    return cf_template";
+                } else {
+                    template.template = '<#import "/uccm/utils/ftl/dictionary.ftl" as dict>\n' +
+                        '{\n' +
+                        '    "kind": "Deployment",\n' +
+                        '    "apiVersion": "extensions/v1beta1",\n' +
+                        '    "metadata": {\n' +
+                        '    "name": "${deployed.name}-depl",\n' +
+                        '    "labels": {\n' +
+                        '        "application": "${application}",\n' +
+                        '        "version": "${version}"\n' +
+                        '        "environment": "${dict.resolve(\'env\', \'test2\')}"\n' +
+                        '    }\n' +
+                        '}'
                 }
+
+                $scope.currentTemplate = template;
+                $scope.setAceMode();
                 $scope.view_mode = "edit";
             };
-
-            $scope.aceLoaded = function(_editor) {
-                // Options
-                //_editor.setReadOnly(true);
-            };
-
-            $scope.aceChanged = function(e) {
-                //
-            };
-
 
             $scope.loadTemplateNames = function() {
                 $http.get('/api/extension/uccm/templates').
@@ -45,11 +61,16 @@ angular
                 $scope.loadTemplateNames();
             };
 
+            $scope.setAceMode = function() {
+                let mode = $scope.currentTemplate.type ==  'py' ? 'python' : 'ftl';
+                $scope.aceOption = angular.extend({}, $scope.aceOption, {'mode': mode});
+            };
 
-            $scope.editTemplate = function(name) {
-                $http.get('/api/extension/uccm/templates?name=' + name).
+            $scope.editTemplate = function(template) {
+                $http.get('/api/extension/uccm/templates?name=' + template.name + '&type=' + template.type).
                 then(function(data) {
                         $scope.currentTemplate = data.data.entity;
+                        $scope.setAceMode();
                         $scope.view_mode = "edit";
                     },
                     function(data) {
